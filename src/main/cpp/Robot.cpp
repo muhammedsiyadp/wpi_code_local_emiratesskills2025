793fd4d70c97e3ea6985cbd444026204068ce995
muhammedsiyadp <muhammedsiyadofficial@gmail.com>
2025-10-26 09:28:33 +0400

    added structure of autonomy to the code

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -12,10 +12,8 @@
 
 int main() { 
 
-// Start MockDS
     Robot r;
     r.ds.Enable();
-
     Hardware hard;
     Movement move( &hard );
     Oms oms( &hard );
@@ -27,6 +25,13 @@ int main() {
     std::thread sharp_thread(&Hardware::Update_sharp_sensors_background, &hard); // Start the sharp sensors update in a separate thread
     delay(1000);
 
+
+    void set_running_lights(bool running){
+        hard.SetRunningLED(running);
+        hard.SetStoppedLED(!running);
+    }
+
+    // section for add your autonomous functions
     void start_to_court_midpoint(){
         move.SetPosition( 30, 35, 0 ); 
         oms.OpenGripper();
@@ -34,155 +39,100 @@ int main() {
         move.PositionDriver(100,100,0);
     }
 
-hard.SetRunningLED(false);
-hard.SetStoppedLED(true);
-
-while(1){
-
-   1. /////////////
-
-    manual code
-
-    ///////////
+    void autonomous_mode(){
+        // put your autonomous code here
+        move.autonomous_mode = true;
+        set_running_lights(true);
 
 
-    autnomous code()
+    }
+    // end of autonomous functions section
+
+    // section for adding your simple tasks functions
+    void simple_task_1(){
+        // Code for simple task 1
+    }
+
+    void simple_task_2(){
+        // Code for simple task 2
+    }
+
+    void simple_task_3(){
+        // Code for simple task 3
+    }
+    // end of simple tasks functions section
 
 
-    //////////////////
+    int function_index = 0;
+    int no_of_functions = 5;
+    frc::SmartDashboard::PutString("Function", "Autonomous Mode");
+    while(1){
+        if (!hard.GetResetButton()){
+            function_index = (function_index + 1) % no_of_functions;
+            if (function_index == 0){
+                frc::SmartDashboard::PutString("Function", "Autonomous Mode");
+            }
+            else if (function_index == 1){
+                frc::SmartDashboard::PutString("Function", "Teleop Drive Mode");
+            }
+            else if (function_index == 2){
+                frc::SmartDashboard::PutString("Function", "Camera Mode");
+            }
+            else if (function_index == 3){
+                frc::SmartDashboard::PutString("Function", "Simple Task 1");
+            }
+            else if (function_index == 4){
+                frc::SmartDashboard::PutString("Function", "Simple Task 2");
+            }
+            else if (function_index == 5){
+                frc::SmartDashboard::PutString("Function", "Simple Task 3");
+            }
+            delay(400);
+        }
+        if (!hard.GetStartButton()){
+            delay(400);
 
+            if (function_index == 0){ //run the autonomous function
+                // Function 1
+            }
+            else if (function_index == 1){ //run the manual drive function
+                while (hard.GetStopButton()){
+                    set_running_lights(true);
+                    move.autonomous_mode = false;
+                    drive.Execute();
+                    delay(50);
+                }
+                set_running_lights(false);
+            }
+            else if (function_index == 2){ //run the camera function
+                // Function 3
+            }
+            else if (function_index == 3){ //run the simple task one
+                set_running_lights(true);
+                simple_task_1();
+                set_running_lights(false);
+            }
+            else if (function_index == 4){  //run the simple task two
+                set_running_lights(true);
+                simple_task_2();
+                set_running_lights(false);
+            }
+            else if (function_index == 5){  //run the simple task three
+                set_running_lights(true);
+                simple_task_3();
+                set_running_lights(false);
+                // Function 6
+            }
 
-    camera
-
-
-    //////////////////
-
-    simple task one
-
-
-    //////
-
-    sim task two
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Wait until the Start Button is pressed
+        }
+        delay(200);
+    
         
-        //delay(50);
-     while( hard.GetStartButton() ){delay(150); }
-    while (false){
-        move.autonomous_mode = false;
-        drive.Execute();
-        delay(50);
-    }
-    
+        
+        
 
-    hard.SetRunningLED(true);
-    hard.SetStoppedLED(false);
-    
-    
-    start_to_court_midpoint();
-    
-    
-    //from court midpoint to shelf midpoint
-    move.PositionDriver(53,135,90);
-
-    oms.elevator_set_height(6.7);
-
-    //from shelf midpoint to first column
-    move.PositionDriver(26,162,90);
-    move.sensor_drive(16 , "front_l");
-    move.sensor_drive(52 , "right");
-    move.sensor_drive(16 , "front_l");
-    move.SetPosition(26,162,90);
-    move.line_align("right");
-
-
-    //pick and come to court centre
-    move.DriveStraight(10);
-    oms.CloseGripper();
-    delay(1000);
-    move.DriveStraight(-12);
-    move.PositionDriver(100,100,0);
-
-    oms.elevator_set_height(12);
-
-    //center to stand to first row (right to left)
-    move.PositionDriver(160,80,0);
-    move.sensor_drive(50 , "right");
-    move.sensor_drive(10 , "front_l");
-    move.SetPosition(160,80,0);
-    move.line_align("left");
-
-    //place and go back
-    move.DriveStraight(11);
-    oms.OpenGripper();
-    delay(2000);
-    move.DriveStraight(-13);
-    move.PositionDriver(100,100,0);
-
-    oms.elevator_set_height(18.5);
-
-    //from court midpoint to shelf midpoint
-    move.PositionDriver(53,135,90);
-
-    //from shelf midpoint to first column
-    move.PositionDriver(26,162,90);
-    move.sensor_drive(16 , "front_l");
-    move.sensor_drive(52 , "right");
-    move.sensor_drive(16 , "front_l");
-    move.SetPosition(26,162,90);
-    move.line_align("right");
-
-     //pick and come to court centre
-    move.DriveStraight(10);
-    oms.CloseGripper();
-    delay(1000);
-    move.DriveStraight(-12);
-    move.PositionDriver(100,100,0);
-
-    oms.elevator_set_height(18.5);
-
-    //center to stand to first row (right to left)
-    move.PositionDriver(160,80,0);
-    move.sensor_drive(50 , "right");
-    move.sensor_drive(10 , "front_l");
-    move.SetPosition(160,80,0);
-    move.line_align("left");
-
-    //place and go back
-    move.DriveStraight(11);
-    oms.OpenGripper();
-    delay(2000);
-    move.DriveStraight(-13);
-    move.PositionDriver(100,100,0);
-
-
-
-
-    
-
-
-    delay(3000);
-
-    
-    while (false){
-        move.autonomous_mode = false;
-        drive.Execute();
-        delay(50);
-    }
-     
+        
+        
     }
     return 0; 
 }
